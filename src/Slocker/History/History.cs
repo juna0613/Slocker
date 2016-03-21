c8741748b1c01f915296e85399fcf0e2247d9779
juna0613 <yuta.naito@gmail.com>
2016-03-21 11:53:55 +0900

    add basic history component

---
+++ src/Slocker/History/History.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slocker
+{
+    public class History
+    {
+        public string Id { get; set; }
+        public Message Message { get; set; }
+        public IEnumerable<string> TargetSources { get; set; }
+    }
+}
---
+++ src/Slocker/History/HistoryManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slocker
+{
+    public class HistoryManager
+    {
+        protected readonly IHistoryCondition _cond;
+        protected readonly IHistoryFetcher _fetcher;
+        protected readonly IMessageParser _parser;
+        public virtual IEnumerable<History> Load()
+        {
+            var data = _fetcher.Fetch(_cond, _parser);
+            return data;
+        }
+    }
+}
