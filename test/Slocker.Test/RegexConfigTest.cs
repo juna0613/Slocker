a14f40c00805f9879ed17d3730b9c3036ffb8bc3
juna0613 <yuta.naito@gmail.com>
2016-04-03 14:51:52 +0900

    add test for RegexConfigTest

---
+++ test/Slocker.Test/RegexConfigTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.IO;
+namespace Slocker.Test
+{
+    [TestFixture]
+    public class RegexConfigTest
+    {
+        private string path = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
+        [SetUp]
+        public void Setup()
+        {
+            if(File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+        [TearDown]
+        public void Teardown()
+        {
+            if(File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+        [Test]
+        public void TestSaveAndRead()
+        {
+            var conf = new RegexConfig
+            {
+                BlockComment = "hoge",
+                MiscExpressions = new[] { "bar" },
+                SingleComment = "zoo"
+            };
+
+            conf.Save(path);
+
+            var conf2 = RegexConfigExtensions.Load(path);
+
+            Assert.That(conf.BlockComment, Is.EqualTo(conf2.BlockComment));
+            Assert.That(conf.SingleComment, Is.EqualTo(conf2.SingleComment));
+            Assert.That(conf.MiscExpressions, Is.EquivalentTo(conf2.MiscExpressions));
+        }
+    }
+}
