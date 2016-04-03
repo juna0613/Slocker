83c6e93aeefd82fdab988793b488c8dba61b7a4a
juna0613 <yuta.naito@gmail.com>
2016-04-03 14:42:23 +0900

    serialization for RegexConfig

--- src/Slocker/Settings/RegexConfig.cs
+++ src/Slocker/Settings/RegexConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -31,6 +32,35 @@ namespace Slocker
             return conf.MiscExpressions.Where(x => !string.IsNullOrEmpty(x)).Select(x => new Regex(x));
         }
 
+        public static void Save(this RegexConfig conf, string path)
+        {
+            CreateDirectoryIfNotExist(Path.GetDirectoryName(path));
+            using (var writer = new StreamWriter(path, false))
+            {
+                writer.Write(Newtonsoft.Json.JsonConvert.SerializeObject(conf));
+            }
+        }
 
+        public static RegexConfig Load(string path)
+        {
+            using (var reader = new StreamReader(path))
+            {
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<RegexConfig>(reader.ReadToEnd());
+            }
+        }
+
+        private static void CreateDirectoryIfNotExist(string dir)
+        {
+            var dirs = Path.GetFullPath(dir).Split('\\');
+            var top = dirs.First();
+            foreach(var subdir in dirs.Skip(1))
+            {
+                top = Path.Combine(top, subdir);
+                if(!Directory.Exists(top))
+                {
+                    Directory.CreateDirectory(top);
+                }
+            }
+        }
     }
 }
